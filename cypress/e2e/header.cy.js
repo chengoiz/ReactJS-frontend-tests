2a508a85af3a78ffcdd1b764737d5d550f4130c5
chen <chengoizman@gmail.com>
2025-04-09 20:12:38 +0300

    Add bodyPage test and cleaned code

--- cypress/e2e/header.cy.js
+++ cypress/e2e/header.cy.js
@@ -35,7 +35,7 @@ describe('Header Component Tests', () => {
        headerPage.getMenuButton().should('be.visible')  
      })
      it('should display appropriate elements on small screen', () => {
-       cy.viewport(375, 667) // iPhone 6/7/8
+       cy.viewport(375, 667) // small screen
        headerPage.getNavLink('Learn').should('not.be.visible')
        headerPage.getNavLink('Reference').should('not.be.visible')
        headerPage.getNavLink('Community').should('not.be.visible')
@@ -61,21 +61,16 @@ describe('Header Component Tests', () => {
       it('should change dark mode icon on click', () => {
         cy.viewport(1280, 800)
 
-        // Initial state should show the "Use Dark Mode" button and not "Use Light Mode"
         headerPage.getEnableDarkModeButton().should('be.visible')
         headerPage.getDisableDarkModeButton().should('not.be.visible')
 
-        // Click to enable dark mode
         headerPage.getEnableDarkModeButton().click()
 
-        // Now the "Use Light Mode" button should be visible instead
         headerPage.getDisableDarkModeButton().should('be.visible')
         headerPage.getEnableDarkModeButton().should('not.be.visible')
 
-        // Toggle back to light mode
         headerPage.getDisableDarkModeButton().click()
 
-        // Confirm it reverted
         headerPage.getEnableDarkModeButton().should('be.visible')
         headerPage.getDisableDarkModeButton().should('not.be.visible')
       })
@@ -83,33 +78,26 @@ describe('Header Component Tests', () => {
       it('should toggle dark mode on and off accurately', () => {
         cy.viewport(1280, 800)
       
-        // Initially, should be light mode (html does NOT have class 'dark')
         cy.get('html').should('not.have.class', 'dark')
         headerPage.getEnableDarkModeButton().should('be.visible')
         headerPage.getDisableDarkModeButton().should('not.be.visible')
-      
-        // Enable dark mode
+
         headerPage.getEnableDarkModeButton().click()
       
-        // Assert dark mode is active (class 'dark' added to <html>)
         cy.get('html').should('have.class', 'dark')
         headerPage.getDisableDarkModeButton().should('be.visible')
         headerPage.getEnableDarkModeButton().should('not.be.visible')
       
-        // Optionally check localStorage
         cy.window().then((win) => {
           expect(win.localStorage.getItem('theme')).to.eq('dark')
         })
       
-        // Disable dark mode
         headerPage.getDisableDarkModeButton().click()
       
-        // Assert it's back to light mode
         cy.get('html').should('not.have.class', 'dark')
         headerPage.getEnableDarkModeButton().should('be.visible')
         headerPage.getDisableDarkModeButton().should('not.be.visible')
       
-        // Confirm localStorage updated
         cy.window().then((win) => {
           expect(win.localStorage.getItem('theme')).to.eq('light')
         })
@@ -128,7 +116,7 @@ describe('Header Component Tests', () => {
   
           it('opens the search input when clicking the search icon', () => {
             headerPage.getSearchButton().click()
-            getSearchInput().should('be.visible')
+            headerPage.getSearchInput().should('be.visible')
           })
         })
   
@@ -139,36 +127,20 @@ describe('Header Component Tests', () => {
           })
   
           it('displays the search input with correct placeholder', () => {
-            cy.get('input[type="search"]')
+            headerPage.getSearchInput()
               .should('be.visible')
               .should('have.attr', 'placeholder', 'Search docs')
           })
   
           it('performs a search and shows relevant results', () => {
-            getSearchInput()
-              .should('be.visible')
-              .type('hello')
-              .should('have.value', 'hello')
+            headerPage.typeInSearchInput('hooks')
   
-              getSearchInput().click()
-  
-            cy.get('.DocSearch-Hit-title')
-              .should('exist')
-              .then(($titles) => {
-                const hasMatch = [...$titles].some((el) =>
-                  el.textContent.toLowerCase().includes('hello')
-                )
-                expect(hasMatch).to.be.true
-              })
+            headerPage.assertSearchHasResultsContaining('hooks')
           })
 
-          it('performs a search and shows relevant results', () => {
-            getSearchInput()
-              .should('be.visible')
-              .type('{enter}')
-              .should('have.value', '')
-  
-              getSearchInput().click()
+          it('performs a search and shows no results after clearing', () => {
+            headerPage.typeInSearchInput('{enter}')
+            headerPage.getSearchInput().click()
   
             cy.get('.DocSearch-Hit-title')
               .should('not.exist')
